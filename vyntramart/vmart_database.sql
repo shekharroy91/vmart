674f8bf8c8f064138137e03bdda21f510fa9c602
shekharroy91 <44096365+shekharroy91@users.noreply.github.com>
2020-05-21 09:59:37 +0530

    Add files via upload

---
+++ vyntramart/vmart_database.sql
@@ -0,0 +1,109 @@
+-- MySQL dump 10.13  Distrib 8.0.18, for Win64 (x86_64)
+--
+-- Host: localhost    Database: vmart
+-- ------------------------------------------------------
+-- Server version	5.7.23
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!50503 SET NAMES utf8 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Table structure for table `auth_role`
+--
+
+DROP TABLE IF EXISTS `auth_role`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `auth_role` (
+  `auth_role_id` int(11) NOT NULL AUTO_INCREMENT,
+  `role_name` varchar(255) DEFAULT NULL,
+  `role_desc` varchar(255) DEFAULT NULL,
+  PRIMARY KEY (`auth_role_id`)
+) ENGINE=MyISAM AUTO_INCREMENT=4 DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `auth_role`
+--
+
+LOCK TABLES `auth_role` WRITE;
+/*!40000 ALTER TABLE `auth_role` DISABLE KEYS */;
+INSERT INTO `auth_role` VALUES (1,'SUPER_USER','This user has ultimate rights for everything'),(2,'ADMIN_USER','This user has admin rights for administrative work'),(3,'SITE_USER','This user has access to site, after login - normal user');
+/*!40000 ALTER TABLE `auth_role` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `auth_user`
+--
+
+DROP TABLE IF EXISTS `auth_user`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `auth_user` (
+  `auth_user_id` int(11) NOT NULL AUTO_INCREMENT,
+  `first_name` varchar(255) NOT NULL,
+  `last_name` varchar(255) NOT NULL,
+  `email` varchar(255) NOT NULL,
+  `password` varchar(255) NOT NULL,
+  `status` varchar(255) DEFAULT NULL,
+  PRIMARY KEY (`auth_user_id`)
+) ENGINE=MyISAM AUTO_INCREMENT=10 DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `auth_user`
+--
+
+LOCK TABLES `auth_user` WRITE;
+/*!40000 ALTER TABLE `auth_user` DISABLE KEYS */;
+INSERT INTO `auth_user` VALUES (1,'shekhar','marella','admin@gmail.com','$2a$10$DD/FQ0hTIprg3fGarZl1reK1f7tzgM4RuFKjAKyun0Si60w6g3v5i','VERIFIED'),(5,'sachin','tendulkar','sachin@gmail.com','$2a$10$7B6lEJ4JDUgc11YN/Lu1y.p9CNMrZPQm8U27J6wGJAY6zlUdQdjwC','VERIFIED'),(9,'anvith','roy','anvithl93@gmail.com','$2a$10$6hIU04MKQbq80cjgX8l9LeW1vr1JnFNg9lVoEFP3ocPaCi2e8c.ZG','VERIFIED');
+/*!40000 ALTER TABLE `auth_user` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `auth_user_role`
+--
+
+DROP TABLE IF EXISTS `auth_user_role`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `auth_user_role` (
+  `auth_user_id` int(11) NOT NULL,
+  `auth_role_id` int(11) NOT NULL,
+  PRIMARY KEY (`auth_user_id`,`auth_role_id`),
+  KEY `FK_user_role` (`auth_role_id`)
+) ENGINE=MyISAM DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `auth_user_role`
+--
+
+LOCK TABLES `auth_user_role` WRITE;
+/*!40000 ALTER TABLE `auth_user_role` DISABLE KEYS */;
+INSERT INTO `auth_user_role` VALUES (1,1),(1,2),(1,3),(2,3),(3,3),(4,3),(5,3),(6,3),(7,3),(8,3),(9,3);
+/*!40000 ALTER TABLE `auth_user_role` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Dumping events for database 'vmart'
+--
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2020-05-20 19:19:18
